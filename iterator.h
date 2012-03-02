ad8370d57cf4368159871e2a8ddb45b9291df52c
technik <cjtech90@gmail.com>
2012-03-03 00:34:04 +0100

    Base iterator template struct

--- iterator.h
+++ iterator.h
@@ -7,8 +7,25 @@
 #ifndef _STL_ITERATOR_H_
 #define _STL_ITERATOR_H_
 
+#include <cstddef>
+
 namespace std
 {
+	// Generic iterator template class
+	template < class Category
+		, class T
+		, class Distance = ptrdiff_t
+		, class Pointer = T*
+		, class Reference = T& >
+		struct iterator
+	{
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+		typedef Category	iterator_category;
+	};
+
 }	// namespace std
 
 #endif // _STL_ITERATOR_H_
