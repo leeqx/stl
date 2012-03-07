eae0242f19f5a5bad8442b78e72ea04916b36dad
technik <cjtech90@gmail.com>
2012-03-07 19:57:17 +0100

    Created vector header

---
+++ vector.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Standard templates library. Freestanding implementation.
+// Created by Carmelo J. Fernández-Agüera Tortosa, a.k.a. Technik
+// On March 7th, 2012
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _STL_VECTOR_H_
+#define _STL_VECTOR_H_
+
+#include <memory.h>
+
+namespace std
+{
+	template < class T, class allocatorT = std::allocator<T> >
+	class vector
+	{
+	};
+}	// namespace std
+
+#endif // _STL_VECTOR_H_
