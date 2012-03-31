316792dcfb3361aa03be72d9af720949b7ee6c3f
technik <cjtech90@gmail.com>
2012-03-31 22:44:13 +0200

    Core of dictionary implemented. Yet to standarize its interface

--- dictionary.h
+++ dictionary.h
@@ -26,7 +26,7 @@ namespace rtl
 		typedef	typename rtl::allocator_traits<allocatorT>::size_type		size_type;
 		typedef typename rtl::allocator_traits<allocatorT>::difference_type	difference_type;
 
-		typedef rtl::pair<const char*, T>		slotT;
+		typedef rtl::pair<char*, T>				slotT;
 		typedef rtl::vector<slotT,allocatorT>	bucketT;
 
 	public:
@@ -46,12 +46,13 @@ namespace rtl
 		bool			empty		() const		{ 0 == mSize; }
 
 		T&				find		(const char * _key);
-		T&				operator	(const char * _key);
+		T&				operator[]	(const char * _key);
 		bool			contains	(const char * _key);
 
 	private:
 		static unsigned					hash	(const char * _key);
-		static bool						keycomp	(const char * _a, const char * _b);
+		static bool						keyComp	(const char * _a, const char * _b);
+		static void						keyCopy	(char *& _dst, const char * _src);
 
 	private:
 		size_type			mSize;
@@ -119,6 +120,96 @@ namespace rtl
 		}
 		// Found nothing, create a new entry
 		// Create a new slot
+		slotT slot(0,T());
+		keyCopy(slot.first, _key);
 		// Push it into the bucket
+		bucket.push_back(slot);
+		return slot.second;
 	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	template<class T, unsigned nb1, class allocatorT>
+	T& dictionary<T,nb1,allocatorT>::operator[](const char * _key)
+	{
+		unsigned bucketId = hash(_key);
+		bucketT& bucket = mBuckets[bucketId];
+		for (bucketT::iterator i = 0; i != bucket.end(); ++i)
+		{
+			if(keyComp(iterator->first,_key))
+			{
+				return iterator->second;
+			}
+		}
+		// Found nothing, create a new entry
+		// Create a new slot
+		slotT slot(0,T());
+		keyCopy(slot.first, _key);
+		// Push it into the bucket
+		bucket.push_back(slot);
+		return slot.second;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	template<class T, unsigned nb1, class allocatorT>
+	bool dictionary<T,nb1,allocatorT>::contains(const char * _key)
+	{
+		unsigned bucketId = hash(_key);
+		bucketT& bucket = mBuckets[bucketId];
+		for (bucketT::iterator i = 0; i != bucket.end(); ++i)
+		{
+			if(keyComp(iterator->first,_key))
+			{
+				return true;
+			}
+		}
+		// Found nothing
+		return false;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	template<class T, unsigned nb, class allocatorT>
+	unsigned dictionary<T,nb1,allocator>::hash(const char * _key)
+	{
+		if(!_key)
+			return 0;
+		unsigned i = 0;
+		unsigned lHash = 0;
+		while(_key[i] != '\0')
+		{
+			lHash += _key[i];
+			++i;
+		}
+		return lHash % nb;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	template<class T, int nb, class allocatorT>
+	bool dictionary<T,nb,allocatorT>::keyComp(const char * _a, const char * _b)
+	{
+		if (!_a || !_b)
+			return false;
+		unsigned i = 0;
+		while(_a[i] != '\0' && _b[i] != '\0')
+		{
+			if(_a[i] != _b[i])
+				return false;
+			++i;
+		}
+		return _a[i] == _b[i];
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	template<class T, int nb, class allocatorT>
+	void dictionary<T,nb,allocatorT>::keyCopy(char *& _dst, const char * _src)
+	{
+		for(unsigned i = 0; _src[i] != '\0'; ++i)
+			;
+		_dst = new char[i+1];
+		_dst[i] = '\0';
+		for(i = 0; _src[i] != '\0'; ++i)
+		{
+			_dst[i] = _src[i];
+		}
+	}
+
 }	// namespace rtl
